09df4b887b16129602ab856c8b90b8201590849c
Suleyman MAMMADOV <suleyman.mammadov@socardownstream.az>
2023-10-02 15:05:19 +0400

    Updated 02.10.23

---
+++ Relationships/Program.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore;
+ECommerceDbContext context = new();
+
+
+
+
+#region Relationships Terimleri 
+
+#region Principal Entity (Asil Entity)
+//Kendi basina var olbilen tabloyu modelleyen entity'e denir. 
+
+//Departmanlar tablosunu modelleyen 'Departman' entity'sidir.
+
+#endregion
+
+#region Dependent Entity(Bagimli Entity)
+//Kendi basina var olmayan, bir baska tabloya bagimli(iliskisel olarak bagimli) olan tabloyu modelleyen entity'e denir
+
+//Calisanlar tablosunu modelleyen 'Calisan' entity'sidir. 
+
+
+
+#endregion
+
+#region Foreign Key
+//Principal Entity ile Dependent Entity arasindaki iliskiyi saglayan key'dir  
+
+//Dependent Entity'de tanimlanir 
+//Principal Entity'de ki Principal Key'i tutar.
+
+#endregion
+
+#region Principal Key
+//Principal Entity'deki id'nin kendisidir
+//Principal Entity'nin kimligi olan kolonu ifade eden propertydir.
+#endregion
+
+
+#region Navigation Property
+
+//Iliskisel tablolar arasindaki fiziksel erisimi entity class'lari uzerinden saglayan property'lerdir
+
+//Bir property'nin navigation property olabilmesi icin kesinlikle entity turunden olmasi gerekiyor. 
+
+
+
+#endregion
+#endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class ECommerceDbContext : DbContext
+{
+  
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server =.; Database=IsletmeDB; Trusted_Connection=True;TrustServerCertificate=True");
+
+        
+    }
+
+
+}
+
+
+public class Calisan
+{
+    public int Id { get; set; }
+    public string CalisanAdi {get; set; }
+    public int DepartmanId { get; set; }    
+
+    public Departman Departman {  get; set; } //Navidation Property
+}
+public class Departman
+{
+    public int Id { get; set; }
+
+    public string DepartmanAdi { get; set; }
+
+    public ICollection<Calisan> Calisanlar { get; set; }    
+}
--- Tracking/Program.cs
+++ Tracking/Program.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System.Xml.Schema;
-
 ECommerceDbContext context = new();
 
 #region DB ADDING VALUES 
@@ -38,6 +36,24 @@ ECommerceDbContext context = new();
 //    Email = "2000@gmail.com"
 //};
 //await context.Kullanicilar.AddRangeAsync(kul1, kul2, kul3);
+//await context.SaveChangesAsync();
+
+
+//Yazar yaz1 = new()
+//{
+//    YazarAdi = "Cem Yilmaz"
+//};
+//Yazar yaz2 = new()
+//{
+//    YazarAdi = "Can Yilmaz"
+//};
+//Yazar yaz3 = new()
+//{
+//    YazarAdi = "Ilber Ortayli"
+//};
+//await context.Yazarlar.AddRangeAsync(yaz1, yaz2, yaz3);
+//await context.SaveChangesAsync();
+
 
 #endregion
 
@@ -47,15 +63,14 @@ ECommerceDbContext context = new();
 
 
 
-await context.SaveChangesAsync();
 
 
 
 #region AsNoTracking Metodu
 //Context uzerinden gelen tum datalar Change Tracker makanizmasi tarafindan takip edilmektedir
 
-//Change Tracker, takip ettigi nesnelerin sayisiyla dogru orantili olacak sekilde bir maliyete sahiptir. O yuzden islem yapilmayacak verilerin tekip edilmesi bizlere 
-//luzumsuz yere bir maliyet ortaya cikaracaktir.
+//Change Tracker, takip ettigi nesnelerin sayisiyla dogru orantili olacak sekilde bir maliyete sahiptir. O yuzden uzerinde islem yapilmayacak verilerin tekip edilmesi
+//bizlere luzumsuz yere bir maliyet ortaya cikaracaktir.
 
 //AsNoTracking metodu, context uzerinden sorgu neticesinde gelecek olan verilerin Change Tracker tarafindan takip edilmesini engeller. 
 
@@ -65,27 +80,64 @@ await context.SaveChangesAsync();
 //AsNoTracking fonksiyonu ile yapilan sorgulamalarda, verileri elde edebilir, bu verileri istenilen noktalarda kullanabilir lakin veriler uzerinde herhangi bir 
 //degisiklik/update islemi yapamayiz. 
 
-var kullanicilar = await context.Kullanicilar.ToListAsync();
+//var kullanicilar = await context.Kullanicilar.AsNoTracking().ToListAsync();
 
-foreach(var kullanici in kullanicilar)
-{
-    Console.Write(kullanici.Adi);
-    kullanici.Adi = $"yeni-{kullanici.Adi}";
-}
+//foreach(var kullanici in kullanicilar)
+//{
+//    Console.Write(kullanici.Adi);
+//    kullanici.Adi = $"yeni-{kullanici.Adi}";
+//    //context.Kullanicilar.Update(kullanici); -> Tracking olmadan database sistemini update etmek 
+//}
+// await context.SaveChangesAsync(); -> Takipli bir sekilde database sistemini update etmek
 
 #endregion
-
-
-await context.SaveChangesAsync();
-
 
 
 #region AsNoTrackingWithIdentifyResolution
+//CT mekanizmasi yinelenen verileri tekil instance olarak getirir. Burdan ekstradan bir performans kazanci soz konusudur
+
+//Bizler yaptigimiz sorgularda takip mekanizmasinin AsNoTracking metodu ile maliyeti kirmak isterken bazen maliyete sebebiyet verebiliriz.(Ozellikle iliskisel tablolari
+//Sorgularken bu durumlara dikkat etmemiz gerekiyor)
+
+//AsNoTracking ile elde edilen veriler takip edilmeyeceginden dolayi yenilenen verilerin ayri instancelarda olmasina sebebiyet veriyoruz. Cunku CT mekanizmasi takip ettigi
+//nesneden bellekte varsa eger ayni nesneden birdaha olusturma geregi duymaksizin o nesneye ayri noktalardaki ihtiyaci ayni instance uzerinden gidermektedir.
+
+//Boyle bir durumda hem takip mekanizmasinin maliyetini ortadan  kaldirmak hemde yenilenen datalari tek bir instance uzerinde karsilamak icin AsNoTrackingWithIdentityResolution
+//fonksiyonunu kullanabiliriz  
+
+
+//var kitaplar =await context.Kitaplar.Include(k => k.Yazarlar).AsNoTrackingWithIdentityResolution().ToListAsync();
+
+
+//AsNoTrackingWithIdentityResolution fonksiyionu AsNoTracking fonksiyonuna nazaran yavastir/maliyetlidir lakin ct'a nazaran daha performansli ve daha az maaliyetlidir
+
+
+
+
+#endregion
+
+
+#region AsTracking
+
+//var kitaplar = await context.Kitaplar.AsTracking().ToListAsync();
+
+//Context uzerinden gelen datalarin ct tarafindan takip edilmesini iradeli bir sekilde ifade etmemizi saglayan fonksiyondur.
+
+
+//UseQueryTrackingBehavior metodunun davranisi geregi uygulama seviyesinde Ct'nin default olarak devrede olup olmamasini ayarliyor olacagiz. 
+//eger ki default olarak pasif hale getirilirse boyle durumda takip mekanizmasinin ihtiyac oldugu sorgularda AsTracking fonskiyonunu kullanabilir ve boylece
+//takip mekanizmasini iradeli bir sekilde devreye sokmus oluruz. 
+
 
 #endregion
 
 
 
+#region UseQueryTrackingBehavior
+//Ef core seviyesinde/uygulama seviyesinde ilgili contextten gelen verilerin uzerinde CT mekanizmasinin davranisi temel seviyede belirlememizi saglayan fonksiyondur. 
+//Yani konfigurasyon fonksiyonudur.
+
+#endregion
 
 
 Console.WriteLine();
@@ -101,7 +153,11 @@ public class ECommerceDbContext : DbContext
     {
         optionsBuilder.UseSqlServer(
             "Server =.; Database=KutuphaneDB; Trusted_Connection=True;TrustServerCertificate=True");
+
+        optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
     }
+
+
 }
 
 
