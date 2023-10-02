345aa2e4e2efda296baa6590169eb5a81be5ed01
Suleyman MAMMADOV <suleyman.mammadov@socardownstream.az>
2023-10-02 17:05:56 +0400

    Updated at the end of the Day

---
+++ One_To_One_Relationship/Program.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+ESirketDbContext context = new();
+
+
+#region Default Convention 
+//Her iki entity'de Navigation property ile birbirlerini tekil olarak referans edereik fiziksel
+//bir iliskinin olacagi ifade edilir. 
+
+//One to One iliski turunde, dependent entity'nin hangisi oldugunu default olarak belirleyebilmek pek kolay degildir. Bu durumda fiziksel olarak bir foreign key'e karsilik
+//property/kolon tanimlayarak cozum getirebiliyoruz
+//Boylece foreign key'e karsilik property tanimlayarak luzumsuz bir kolon olusturmus oluyoruz. 
+//public class Calisan
+//{
+//    public int Id { get; set; }
+//    public string Adi { get; set; }
+
+//    public CalisanAdresi CalisanAdresi { get; set; }
+//}
+//public class CalisanAdresi
+//{
+//    public int Id { get; set; }
+
+//    public int CalisanId { get; set; }  
+//    public string Adres { get; set; }
+
+//    public Calisan Calisan { get; set; }
+
+//}
+
+#endregion
+
+#region Data Annotations  
+
+//Navigation Property'ler tanimlanmalidir 
+//Foreign kolonunun ismi default conventionun dusunda bir kolon olacaksa eger ForignKey attribute ile bunu bildirebiliriz
+//Foreign Key kolonu olusturulmak zorunda degildir
+
+//1 e 1 iliskide ekstradan foreign key kolonuna ihtiyac olmayacagindan dolayi dependent entity'deki id kolonunun hem forign key hem de primary key olarak kullanmayi 
+//tercih ediyoruz ve bu duruma ozen gosterimelidir diyoruz. 
+
+
+
+//public class Calisan
+//{
+//    public int Id { get; set; }
+//    public string Adi { get; set; }
+
+//    public CalisanAdresi CalisanAdresi { get; set; }
+
+
+
+//}
+//public class CalisanAdresi
+//{
+//    [Key, ForeignKey(nameof(Calisan))]
+//    public int Id { get; set; }
+
+//    public string Adres { get; set; }
+
+//    public Calisan Calisan { get; set; }
+
+//}
+
+#endregion
+
+#region Fluent API 
+
+
+public class Calisan
+{
+    public int Id { get; set; }
+    public string Adi { get; set; }
+
+    public CalisanAdresi CalisanAdresi { get; set; }
+
+
+
+}
+public class CalisanAdresi
+{
+    [Key, ForeignKey(nameof(Calisan))]
+    public int Id { get; set; }
+
+    public string Adres { get; set; }
+
+    public Calisan Calisan { get; set; }
+
+}
+
+#endregion
+
+
+
+
+
+
+public class ESirketDbContext : DbContext
+{
+    DbSet<Calisan> Calisanlar { get; set; }
+    DbSet<CalisanAdresi> CalisanAdresleri { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server =.; Database=ESirketDb; Trusted_Connection=True;TrustServerCertificate=True");
+    }
+    //Model'larin (entity) Veritabaninda generate edilecek yapilan konfigurasyonlari bu fonskiyon icerisinde konfigure edilir. 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CalisanAdresi>()
+            .HasKey(c => c.Id);
+        modelBuilder.Entity<Calisan>()
+            .HasOne(c => c.CalisanAdresi)
+            .WithOne(c => c.Calisan)
+            .HasForeignKey<CalisanAdresi>(c => c.Id);
+    }
+}
+
+
--- Relationships/Program.cs
+++ Relationships/Program.cs
@@ -34,7 +34,7 @@ ECommerceDbContext context = new();
 //Principal Entity'deki id'nin kendisidir
 //Principal Entity'nin kimligi olan kolonu ifade eden propertydir.
 #endregion
-
+#endregion
 
 #region Navigation Property
 
@@ -42,15 +42,66 @@ ECommerceDbContext context = new();
 
 //Bir property'nin navigation property olabilmesi icin kesinlikle entity turunden olmasi gerekiyor. 
 
+//Navigation Property'ler entity'lerdeki tanimlarina gore n'e n yahut 1'e n seklinde iliski turlarini ifade etmektedirler 
+
+#endregion
+
+#region Iliski Turleri
+
+#region One to One 
+//Calisan ile adresi arasindaki iliski
+//Kari koca arasindaki iliski 
+#endregion
+
+#region One to Many 
+//Calisan ile departman arasindaki iliski
+//Anne ve cocuklari arasindaki iliski
+#endregion
+
+#region Many to Many 
+//Calisanlar ile proje arasindaki iliski
+//Kardesler arasindaki iliski 
+#endregion
+
+
+
 
 
 #endregion
+
+#region Iliski Yapilandirma Yontemleri
+
+#region Default Conventions 
+//Varsayilan entity kurallarini kullanarak yapilan iliski yapilandirma yontemleridir.  
+
+//Navigation property'leri kullanarak iliski sablonlarini cikarmaktadir. 
 #endregion
 
+#region Data Annotations Attribtes
+//Entity'nin niteliklerine gore ince ayarlar yapmamizi saglayan attribute'lardir [Key] , [ForeignKey]
+#endregion
+
+#region Fluent API 
+
+//Entity modellerindeki iliskileri yapilandirirken daha detayli calismamizi saglayan yontemdir.  
 
 
+#endregion
 
+#region HasOne
+//Ilgili entity'nin iliskisel entity'ye birebir ya da bire cok olacak sekilde iliskisini yapilandirmaya baslayan metottur 
+#endregion
 
+#region HasMany
+//Ilgili entity'nin iliskisel entity'ye  bire cok  ya da coka cok olacak sekilde  iliskisini yapilandirmaya baslayan metottur
+#endregion
+
+#region WithMany
+//HasOne ya da HasMany'den sonra bire cok ya da coka cok olacak sekilde iliski yapilandirmasini tamamlayan metottur. 
+
+#endregion
+
+#endregion
 
 
 
@@ -70,11 +121,7 @@ public class ECommerceDbContext : DbContext
     {
         optionsBuilder.UseSqlServer(
             "Server =.; Database=IsletmeDB; Trusted_Connection=True;TrustServerCertificate=True");
-
-        
     }
-
-
 }
 
 
@@ -84,7 +131,7 @@ public class Calisan
     public string CalisanAdi {get; set; }
     public int DepartmanId { get; set; }    
 
-    public Departman Departman {  get; set; } //Navidation Property
+    public Departman Departman {  get; set; } //Navigation Property
 }
 public class Departman
 {
