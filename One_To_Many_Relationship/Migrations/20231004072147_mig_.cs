92d0978e4a5db1ef89c43cdc72032f117b92e9a8
Suleyman MAMMADOV <suleyman.mammadov@socardownstream.az>
2023-10-04 11:51:52 +0400

    Lesson 23 Finished

---
+++ One_To_Many_Relationship/Migrations/20231004072147_mig_.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace One_To_Many_Relationship.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_ : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departmanlar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmanAdi = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departmanlar", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Calisanlar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DId = table.Column<int>(type: "int", nullable: false),
+                    Adi = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Calisanlar", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Calisanlar_Departmanlar_DId",
+                        column: x => x.DId,
+                        principalTable: "Departmanlar",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Calisanlar_DId",
+                table: "Calisanlar",
+                column: "DId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Calisanlar");
+
+            migrationBuilder.DropTable(
+                name: "Departmanlar");
+        }
+    }
+}
---
+++ One_To_Many_Relationship/Program.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+ESirketDbContext context = new();
+
+#region Default Convention 
+//Default convention yonteminde bire cok iliskiyi kurarken foreign key kolonuna karsilik gelen bir property tanimlamak mecburiyetinde degiliz. Eger tanimlamaz isek Ef Core 
+//bunu kendisini tamamamlayacak. Eger tanimkar  isek, tanimladigimizi baz alacaktir. 
+//class Calisan //Dependent entity (Calisan Departmana Bagli) 
+//{
+//    public int Id { get; set; }
+//    public int DepartmanId { get; set; }    
+//    public string Adi { get; set; }
+
+//    public Departman Departman { get; set; }    
+
+//}
+
+//class Departman //Principal entity 
+//{
+
+//    public int Id { get; set; }
+
+//    public string DepartmanAdi { get; set; }   
+
+//    public ICollection<Calisan> Calisanlar {get; set; }
+//}
+
+
+
+#endregion
+
+#region Data Annotations 
+//ForeignKey Zorunludur 
+//Defult convention yonteminde foreign key kolonuna karsilik gelen property i tanimladigimizda bu property ismi temel geleneksel entity tanimlama kurallarina uymuyorsa eger
+//Data annotations'lar ile mudahalede bulunabiliriz  
+//class Calisan //Dependent entity (Calisan Departmana Bagli) 
+//{
+//    public int Id { get; set; }
+//    [ForeignKey(nameof(Departman))]
+//    public int DId { get; set; }
+//    public string Adi { get; set; }
+
+//    public Departman Departman { get; set; }
+
+//}
+
+//class Departman //Principal entity 
+//{
+
+//    public int Id { get; set; }
+
+//    public string DepartmanAdi { get; set; }
+
+//    public ICollection<Calisan> Calisanlar { get; set; }
+//}
+
+#endregion
+
+#region Fluent API 
+
+class Calisan //Dependent Entity    
+{
+    public int Id { get; set; } 
+
+    public int DId { get; set; } //Ornek olarak koyuldu. Bir spesifik foreignkey olmasa da ef core kendisi belirleyebilir  
+    public string Adi { get; set; }
+
+
+    public Departman Departman { get; set; }
+
+}
+
+class Departman //Principal entity 
+{
+
+    public int Id { get; set; }
+
+    public string DepartmanAdi { get; set; }
+
+    public ICollection<Calisan> Calisanlar { get; set; }
+}
+
+
+#endregion
+
+
+
+
+
+public class ESirketDbContext : DbContext
+{
+    DbSet<Calisan> Calisanlar { get; set; }
+    DbSet<Departman> Departmanlar { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server =.; Database=ESirketDb; Trusted_Connection=True;TrustServerCertificate=True");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Calisan>()
+            .HasOne(c => c.Departman)
+            .WithMany(d => d.Calisanlar)
+            .HasForeignKey(c => c.DId);
+    }
+}
+
