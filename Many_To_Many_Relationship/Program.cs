788d72e50625e23587a75fdf59bbddd8e2988898
Suleyman MAMMADOV <suleyman.mammadov@socardownstream.az>
2023-10-04 11:52:31 +0400

    Lesson 24 NotFinished

---
+++ Many_To_Many_Relationship/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+Console.Read();
+#region Default Convention 
+
+ public class Kitap
+{
+    public int Id { get; set; } 
+
+    public string KitapAdi { get; set; }
+
+   public  ICollection<Yazar> Yazarlar { get; set; }   
+
+}
+
+public class Yazar
+{ 
+    public int Id { get; set; }
+    public string YazarAdi { get; set; }    
+
+    public ICollection<Kitap> Kitaplar { get; set; }    
+    
+}
+#endregion
+
+
+
+
+
+
+
+public class EKitapDbContext : DbContext
+{
+ 
+    public DbSet<Kitap> Kitaplar { get; set; } 
+
+    public DbSet<Yazar> Yazarlar { get; set; }  
+   
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server =.; Database=EKitapDb; Trusted_Connection=True;TrustServerCertificate=True");
+    }
+
+}
+
